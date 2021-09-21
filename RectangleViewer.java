7ce1fc65276c5be85aa6206e24f4fc5bfde78c68
cwydle <redtriger213221@gmail.com>
2021-09-21 19:22:47 -0400

    new

---
+++ RectangleViewer.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+//import 
+public class RectangleViewer {
+    public static void name() {
+       // JFrame.frame = new JFrame();
+       // frame.setSize(300,400);
+       // frame.setTitle("Two Rectangles");
+       // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       // RectangleComponent component = new RectangleComponent(); 
+        //frame.add(component);
+        //frame.setVisible(true);
+    }
+    
+
+    
+}
